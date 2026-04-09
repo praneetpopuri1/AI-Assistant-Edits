34153af89f46e458b42dc38d1496b1d866abfab4
Praneet Popuri <praneet.popuri1@gmail.com>
2026-04-09 13:40:23 -0500

    testing short clip using QwenVL 8B Thinking

--- pipeline/master_model/QwenVL/test.py
+++ pipeline/master_model/QwenVL/test.py
@@ -1,1 +1,80 @@
-from transformers import AutoProcessor, AutoModelForVision2Seq
+import torch
+from transformers import AutoProcessor, AutoModelForImageTextToText, BitsAndBytesConfig
+from qwen_vl_utils import process_vision_info
+
+
+model_id = "Qwen/Qwen3-VL-8B-Thinking"
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+model = AutoModelForImageTextToText.from_pretrained(
+    model_id,
+    device_map="auto",
+    quantization_config=quant_config,
+    torch_dtype=torch.float16,
+)
+
+
+def inference(video, prompt, max_new_tokens=2048, total_pixels=20480 * 32 * 32, min_pixels=64 * 32 * 32, max_frames= 2048, sample_fps = 2):
+    """
+    Perform multimodal inference on input video and text prompt to generate model response.
+
+    Args:
+        video (str or list/tuple): Video input, supports two formats:
+            - str: Path or URL to a video file. The function will automatically read and sample frames.
+            - list/tuple: Pre-sampled list of video frames (PIL.Image or url). 
+              In this case, `sample_fps` indicates the frame rate at which these frames were sampled from the original video.
+        prompt (str): User text prompt to guide the model's generation.
+        max_new_tokens (int, optional): Maximum number of tokens to generate. Default is 2048.
+        total_pixels (int, optional): Maximum total pixels for video frame resizing (upper bound). Default is 20480*32*32.
+        min_pixels (int, optional): Minimum total pixels for video frame resizing (lower bound). Default is 16*32*32.
+        sample_fps (int, optional): ONLY effective when `video` is a list/tuple of frames!
+            Specifies the original sampling frame rate (FPS) from which the frame list was extracted.
+            Used for temporal alignment or normalization in the model. Default is 2.
+
+    Returns:
+        str: Generated text response from the model.
+
+    Notes:
+        - When `video` is a string (path/URL), `sample_fps` is ignored and will be overridden by the video reader backend.
+        - When `video` is a frame list, `sample_fps` informs the model of the original sampling rate to help understand temporal density.
+    """
+
+    messages = [
+        {"role": "user", "content": [
+                {"video": video,
+                "total_pixels": total_pixels, 
+                "min_pixels": min_pixels, 
+                "max_frames": max_frames,
+                'sample_fps':sample_fps},
+                {"type": "text", "text": prompt},
+            ]
+        },
+    ]
+    text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    image_inputs, video_inputs, video_kwargs = process_vision_info([messages], return_video_kwargs=True, 
+                                                                   image_patch_size= 16,
+                                                                   return_video_metadata=True)
+    if video_inputs is not None:
+        video_inputs, video_metadatas = zip(*video_inputs)
+        video_inputs, video_metadatas = list(video_inputs), list(video_metadatas)
+    else:
+        video_metadatas = None
+    inputs = processor(text=[text], images=image_inputs, videos=video_inputs, video_metadata=video_metadatas, **video_kwargs, do_resize=False, return_tensors="pt")
+    inputs = inputs.to('cuda')
+
+    output_ids = model.generate(**inputs, max_new_tokens=max_new_tokens)
+    generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
+    output_text = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    return output_text[0]
+
+
+answer = inference("../../../short_clips/pranav_monkey.mp4", "Describe the video in detail, including key moments and overall theme.")
+print(answer)
